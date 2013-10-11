8b5ed139dc15ae7c0ef3dd82b27593f510d4b5c0
Carl Schwope <schwope.carl@gmail.com>
2013-10-11 10:15:32 -0400

    refactor background.js

--- extension_chrome/background.js
+++ extension_chrome/background.js
@@ -1,18 +1,30 @@
-chrome.browserAction.onClicked.addListener( function(tab){
+function ClickEvent( tab ) {
+  this.tab = tab;
+  this.url = tab.url;
+  this.id  = tab.id;
+  this.response = undefined;
 
-  chrome.tabs.sendMessage(tab.id, {},
-    function(response){
-      body_html = response.response;
-      chrome.tabs.create({
-          'url': chrome.extension.getURL('results.html'),
-          'active': false
-          },
-          function(new_tab) {
-            var result = generateSearchResults( body_html , tab.url );
-            alert( result );
-          }
-      );
-    }
-  );
+  this.tab_options = {
+    'url': chrome.extension.getURL('results.html'),
+    'active': false
+  };
+}
 
-});
+click_event = undefined;
+
+function onCreateTab( new_tab ){
+  var result = generateSearchResults( click_event.response , click_event.url );
+  alert( result );
+}
+
+function onMessageRecieved( response ){
+  click_event.response = response.response
+  chrome.tabs.create( click_event.tab_options, onCreateTab );
+};
+
+function buttonClick( current_tab ) {
+  click_event = new ClickEvent( current_tab );
+  chrome.tabs.sendMessage(click_event.id, {}, onMessageRecieved );
+}
+
+chrome.browserAction.onClicked.addListener( buttonClick );
