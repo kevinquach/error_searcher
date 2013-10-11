dcd37ca45c00766a108ff95cb6d09812d87f08f8
Carl Schwope <schwope.carl@gmail.com>
2013-10-11 17:43:40 -0400

    new tab shows results of search

--- extension_chrome/query_stackOverflow.js
+++ extension_chrome/query_stackOverflow.js
@@ -1,14 +1,17 @@
+function Thing( word ){
+  this.result = 'word';
+};
+
+mything = new Thing('original');
 
 function processAnswers(data){
   //manage answer data
   var answerBody = data.items[0].body;
-  alert(answerBody);
-  debugger;
+  mything.result = answerBody;
 }
 
 function processQuestionResult(data){
   var questionId = data.items[0].question_id;
-  alert("question id: " + questionId);
   $.get("https://api.stackexchange.com/2.1/questions/"+ questionId + "/answers?order=desc&sort=votes&site=stackoverflow&filter=!bc0qAAU9ADlgOV",
       processAnswers);
 }
@@ -18,11 +21,11 @@ function queryStackOverflow( query )
   // result = query.example;
   // use different members of the query object to
   // create a stack overflow search
-  $.get("http://api.stackexchange.com/2.1/search?page=1&order=desc&sort=relevance&intitle=" + query.example + "&site=stackoverflow",
+  $.get("http://api.stackexchange.com/2.1/search?page=1&order=desc&sort=relevance&intitle=" + "No+route+matches" + "&site=stackoverflow",
     processQuestionResult);
 
   // alert(query);
   // debugger;
   // maybe process result into an object
-
+  return mything.result;
 }
